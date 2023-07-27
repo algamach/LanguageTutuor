17208405e7fc94d8c545267d2d3083aeddd42467
algamach <123144931+algamach@users.noreply.github.com>
2023-07-27 14:37:23 +0300

    Telegram.Bot

--- Program.cs
+++ Program.cs
@@ -1,43 +1,23 @@
 ﻿using System.ComponentModel;
+using Telegram.Bot;
 
 internal class Program
 {
+    static TelegramBotClient Bot;
     private static void Main(string[] args)
     {
-        var nums = GetArrayFromFile("path.txt");
+        string token = File.ReadAllText("token.txt");
+        Bot = new TelegramBotClient(token);
+
+        Bot.OnMessage += Bot_OnMessage;
+        Bot.StartReceiving();
+        Console.ReadLine();
+        Bot.StopReceiving();
     }
-    static int[] GetArrayFromFile(string path)
+
+    private static void Bot_OnMessage(object? sender, Telegram.Bot.Args.MessageEventArgs e)
     {
-        try
-        {
-            string content = File.ReadAllText(path);
-            var numString = content.Split(" ");
-            List<int> nums = new List<int>();
-            for (int i = 0; i < numString.Length; i++)
-            {
-                int num;
-                if (int.TryParse(numString[i],out num))
-                    nums.Add(num);
-            }
-            return nums.ToArray();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Не удалось прочитать файл");
-            return new int[0];
-        }
-    }
-    static int GetRowCount(string path)
-    {
-        try
-        {
-            var rows = File.ReadAllLines(path);
-            return rows.Length;
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Файл по адресу {path} был не найден");
-            return 0;
-        }
+        Console.WriteLine(e.Message.Text);
+        Console.WriteLine(e.Message.From.Username);
     }
 }
